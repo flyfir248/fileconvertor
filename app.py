b3309e67d3de8dce0d0a4e36981ab57432ff55a6
@johnya0 <anoopjohny2000@gmail.com>
2023-01-25 21:03:20 +0100

    to sub branch

--- app.py
+++ app.py
@@ -4,40 +4,33 @@ from PIL import Image
 
 app = Flask(__name__)
 
-
 @app.route("/", methods=["GET"])
 def index():
     return render_template("index.html")
 
-
-@app.route("/jpgtopng.html", methods=["GET"])
+@app.route("/jpgtopng", methods=["GET"])
 def jpgtopng():
     return render_template("jpgtopng.html")
 
-
-@app.route("/pngtojpg.html", methods=["GET"])
+@app.route("/pngtojpg", methods=["GET"])
 def pngtojpg():
     return render_template("pngtojpg.html")
 
-
-@app.route("/webtopng.html", methods=["GET"])
+@app.route("/webtopng", methods=["GET"])
 def webtopng():
     return render_template("webtopng.html")
 
-
-@app.route("/bmptopng.html", methods=["GET"])
+@app.route("/bmptopng", methods=["GET"])
 def bmptopng():
     return render_template("bmptopng.html")
 
-
-@app.route("/pngtopdf.html", methods=["GET"])
+@app.route("/pngtopdf", methods=["GET"])
 def pngtopdf():
     return render_template("pngtopdf.html")
 
-
 @app.route("/api/jpgtopng", methods=["POST"])
 def jpg_to_png():
-    image = request.files.get('image')
+    image= request.files.get('image')
     image_bytes = image.read()
     im = Image.open(io.BytesIO(image_bytes))
     im = im.convert("RGB")
@@ -48,7 +41,6 @@ def jpg_to_png():
     img_buffer.seek(0)
     return Response(img_buffer, content_type="image/png",
                     headers={'Content-Disposition': 'attachment; filename=image.png'})
-
 
 @app.route("/api/pngtojpg", methods=["POST"])
 def png_to_jpg():
@@ -63,7 +55,6 @@ def png_to_jpg():
     return Response(img_buffer, content_type="image/jpeg",
                     headers={'Content-Disposition': 'attachment; filename=image.jpg'})
 
-
 @app.route("/api/webptopng", methods=["POST"])
 def webp_to_png():
     image = request.files.get('image')
@@ -76,7 +67,6 @@ def webp_to_png():
     img_buffer.seek(0)
     return Response(img_buffer, content_type="image/png",
                     headers={'Content-Disposition': 'attachment; filename=image.png'})
-
 
 @app.route("/api/bmptopng", methods=["POST"])
 def bmp_to_png():
@@ -91,7 +81,6 @@ def bmp_to_png():
     return Response(img_buffer, content_type="image/png",
                     headers={'Content-Disposition': 'attachment; filename=image.png'})
 
-
 @app.route("/api/pngtopdf", methods=["POST"])
 def png_to_pdf():
     image = request.files.get('image')
@@ -99,10 +88,12 @@ def png_to_pdf():
     im = Image.open(io.BytesIO(image_bytes))
     # create BytesIO object
     pdf_buffer = io.BytesIO()
+    # save image to the buffer using PDF format
     im.save(pdf_buffer, format='PDF')
     pdf_buffer.seek(0)
     return Response(pdf_buffer, content_type="application/pdf",
                     headers={'Content-Disposition': 'attachment; filename=image.pdf'})
+###############################################
 
 
 if __name__ == '__main__':
