626757a7578abfe25f3afde23d776ae7415abee5
@johnya0 <anoopjohny2000@gmail.com>
2023-01-25 21:39:14 +0100

    to css branch

--- app.py
+++ app.py
@@ -9,6 +9,9 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route("/about.html")
+def about():
+    return render_template("about.html")
 
 @app.route("/jpgtopng.html", methods=["GET"])
 def jpgtopng():
